e5e0b7f5108350b89841a46a195cd444a3d3e66a
Volodymyr 55CancriE Nykonov <cancrigames@gmail.com>
2023-03-26 15:08:05 +0300

    Create empty addon

---
+++ Core/Utils.lua
@@ -0,0 +1,31 @@
+local _, ns = ...;
+
+do
+    if (ns.Utils) then return end
+    local utils = {}
+    ns.Utils = utils
+
+    function utils.GenerateUuid()
+        local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+        return string.gsub(template, '[xy]', function(c)
+            local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+            return string.format('%x', v)
+        end)
+    end
+
+    function utils.GetStringHash(text)
+        local counter = 1
+        local len = string.len(text)
+        for i = 1, len, 3 do
+            counter = math.fmod(counter * 8161, 4294967279) + -- 2^32 - 17: Prime!
+                (string.byte(text, i) * 16776193) +
+                ((string.byte(text, i + 1) or (len - i + 256)) * 8372226) +
+                ((string.byte(text, i + 2) or (len - i + 256)) * 3932164)
+        end
+        return math.fmod(counter, 4294967291) -- 2^32 - 5: Prime (and different from the prime in the loop)
+    end
+end
+
+do
+
+end
---
+++ Database/Init.lua
@@ -0,0 +1,6 @@
+local _, ns = ...;
+
+if (ns.db) then return end
+
+local db = {}
+ns.db = db
