96d21d6a374874c3395d27e28a3fd04f23b7d693
Volodymyr 55CancriE Nykonov <cancrigames@gmail.com>
2023-04-07 11:54:02 +0300

    Add database for npc translations Trello: #23

--- Database/Init.lua
+++ Database/Init.lua
@@ -1,14 +1,8 @@
 local _, ns = ...;
 
-do -- Constants
-    ns.LEVEL_TRANSLATION = "Рівень"
-    ns.PAGE_TRANSLATION = "Сторінка"
-    ns.LEADER_TRANSLATION = "Лідер"
-end
+-- Constants
+ns.LEVEL_TRANSLATION = "Рівень"
+ns.PAGE_TRANSLATION = "Сторінка"
+ns.LEADER_TRANSLATION = "Лідер"
 
-do
-    if (ns._db) then return end
-
-    local db = {}
-    ns._db = db
-end
+ns._db = {}
