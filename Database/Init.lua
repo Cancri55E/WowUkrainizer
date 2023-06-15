3be74a81a5c42b103123cb4c6e23537bf7cee307
55 Cancri E <127979098+Cancri55E@users.noreply.github.com>
2023-06-15 22:04:29 +0300

    fix: add translation for "replaced by" tooltip for talent (#31)

--- Database/Init.lua
+++ Database/Init.lua
@@ -11,6 +11,7 @@ ns.SPELL_RANK_TRANSLATION = "Pівень"
 ns.SPELL_NEXT_RANK_TRANSLATION = "Наступний рівень:"
 
 ns.TALENT_REPLACES_TRANSLATION = "Замінює"
+ns.TALENT_REPLACED_BY_TRANSLATION = "Замінено на"
 ns.TALENT_UPGRADE_TRANSLATION = "Оновлення"
 
 ns.PET_LEVEL_TRANSLATION = "Супутник {1} рівня"
