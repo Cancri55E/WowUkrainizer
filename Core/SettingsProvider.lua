3b7411da26a2ae4e9ed1a0fc9aa1c58d61d01987
Volodymyr 55CancriE Nykonov <cancrigames@gmail.com>
2023-07-14 19:41:35 +0300

    chore: fix name in Contributors page

--- Core/SettingsProvider.lua
+++ Core/SettingsProvider.lua
@@ -514,7 +514,7 @@ function settingsProvider:Build()
                                 "KuprumLight, Mark Tsemma, Glafira, Алексей Коваль, Serhii Feelenko, " ..
                                 "Semerkhet, senpusha, Валерий Бондаренко, NichnaVoitelka, Shelby333, " ..
                                 "Nazar Kulchytskyi, Dmytro Borishpolets, RomenSkyJR, Дмитро Горєнков, " ..
-                                "Женя Браславська, Asturiel, Лігво Друїда, Volodymyr Taras, Олексій Сьомін, " ..
+                                "Женя Браславська, Elanka, Asturiel, Лігво Друїда, Volodymyr Taras, Олексій Сьомін, " ..
                                 "Ксенія Никонова, Primarch, rchenok, Артем Белякін, Roma Rybai, Andrew Kucherov, " ..
                                 "Toris_McDessert"
                         end,
