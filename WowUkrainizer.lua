0a2b5e52e3e72f58fae125e08377b9e773657bc3
Galitsky Dmytro <melles.android@gmail.com>
2023-07-21 23:34:33 +0300

    Instead of individual checks and function calls for each translator, a for loop is now used in conjunction with an array for convenient management of translators.
    Changes made:
    1. Added the "translators" table to store translator data. Each translator is represented as an object with "enabled" (boolean value) and "func" (translation function) fields.
    2. Eliminated code duplication in the function calls of translators by moving the checks into the for loop.
    3. Added comments to the code to explain the new data structures and logic.

--- WowUkrainizer.lua
+++ WowUkrainizer.lua
@@ -3,10 +3,58 @@ local addonName, ns = ...;
 local sharedMedia = LibStub("LibSharedMedia-3.0")
 local settingsProvider = ns.SettingsProvider:new()
 
-local SetFont = ns.FontStringExtensions.SetFont
+local translators = {
+    {
+        name = "ClassTalentFrameTranslator",
+        args = nil,
+        isEnabled = function()
+            return WowUkrainizer_Options
+                .TranslateClassTalentsFrame
+        end
+    },
+    {
+        name = "SpellbookFrameTranslator",
+        args = nil,
+        isEnabled = function()
+            return WowUkrainizer_Options
+                .TranslateSpellbookFrame
+        end
+    },
+    {
+        name = "NameplateAndUnitFrameTranslator",
+        args = nil,
+        isEnabled = function()
+            return WowUkrainizer_Options
+                .TranslateNameplatesAndUnitFrames
+        end
+    },
+    {
+        name = "SpellTooltipTranslator",
+        args = Enum.TooltipDataType.Spell,
+        isEnabled = function()
+            return
+                WowUkrainizer_Options.TranslateSpellTooltips
+        end
+    },
+    {
+        name = "UnitTooltipTranslator",
+        args = Enum.TooltipDataType.Unit,
+        isEnabled = function()
+            return
+                WowUkrainizer_Options.TranslateUnitTooltips
+        end
+    },
+    {
+        name = "MovieTranslator",
+        args = nil,
+        isEnabled = function()
+            return
+                WowUkrainizer_Options.TranslateMovieSubtitles
+        end
+    }
+}
 
-local unitTooltipTranslator, spellTooltipTranslator, spellbookFrameTranslator, classTalentFrameTranslator
-local nameplateAndUnitFrameTranslator, movieTranslator
+local SetFont = ns.FontStringExtensions.SetFont
 
 local initialized = false
 
@@ -25,19 +73,29 @@ local function setGameFonts()
     local useDefaultFonts, fontName, fontScale, tooltipHeaderFontScale, tooltipFontScale =
         settingsProvider.GetFontSettings()
 
-    if (useDefaultFonts) then return end
+    if useDefaultFonts then return end
 
-    -- System fonts
-    SetFont(SystemFont_Shadow_Med1, fontName, fontScale)
-    SetFont(SystemFont_Shadow_Small, fontName, fontScale)
-    SetFont(SystemFont_Shadow_Med2, fontName, fontScale)
-    SetFont(SystemFont_Shadow_Large2, fontName, fontScale)
-    SetFont(Game30Font, fontName, fontScale)
+    local fontElements = {
+        { element = SystemFont_Shadow_Med1,   scale = fontScale },
+        { element = SystemFont_Shadow_Small,  scale = fontScale },
+        { element = SystemFont_Shadow_Med2,   scale = fontScale },
+        { element = SystemFont_Shadow_Large2, scale = fontScale },
+        { element = Game30Font,               scale = fontScale },
+    }
 
-    -- Tooltip fonts
-    SetFont(GameTooltipHeader, fontName, tooltipHeaderFontScale)
-    SetFont(Tooltip_Med, fontName, tooltipFontScale)
-    SetFont(Tooltip_Small, fontName, tooltipFontScale)
+    local tooltipElements = {
+        { element = GameTooltipHeader, scale = tooltipHeaderFontScale },
+        { element = Tooltip_Med,       scale = tooltipFontScale },
+        { element = Tooltip_Small,     scale = tooltipFontScale },
+    }
+
+    for _, fontElement in ipairs(fontElements) do
+        SetFont(fontElement.element, fontName, fontElement.scale)
+    end
+
+    for _, tooltipElement in ipairs(tooltipElements) do
+        SetFont(tooltipElement.element, fontName, tooltipElement.scale)
+    end
 end
 
 local function initializeAddon()
@@ -61,47 +119,30 @@ end
 
 local eventHandler = ns.EventHandler:new()
 
-local function OnAddOnLoaded(_, name)
-    local function OnPlayerLogin()
-        settingsProvider:Load()
-        createInterfaceOptions()
-        setGameFonts();
+local function OnPlayerLogin()
+    settingsProvider:Load()
+    createInterfaceOptions()
+    setGameFonts();
 
-        local translateClassTalentsFrame, translateSpellbookFrame, translateNameplatesAndUnitFrames, translateSpellTooltips, translateUnitTooltips, translateMovieSubtitles =
-            settingsProvider.GetTranslatorsState()
-
-        -- Tooltips
-        if (translateUnitTooltips) then
-            unitTooltipTranslator = ns.Translators.UnitTooltipTranslator:new(Enum.TooltipDataType.Unit)
-            unitTooltipTranslator:SetEnabled(true)
+    local function createTranslator(translatorName, args)
+        local translator
+        if args ~= nil then
+            translator = ns.Translators[translatorName]:new(args)
+        else
+            translator = ns.Translators[translatorName]:new()
         end
-
-        if (translateSpellTooltips) then
-            spellTooltipTranslator = ns.Translators.SpellTooltipTranslator:new(Enum.TooltipDataType.Spell)
-            spellTooltipTranslator:SetEnabled(true)
-        end
-
-        -- Frames
-        if (translateSpellbookFrame) then
-            spellbookFrameTranslator = ns.Translators.SpellbookFrameTranslator:new()
-            spellbookFrameTranslator:SetEnabled(true)
-        end
-
-        if (translateClassTalentsFrame) then
-            classTalentFrameTranslator = ns.Translators.ClassTalentFrameTranslator:new()
-            classTalentFrameTranslator:SetEnabled(true)
-        end
-        -- Other
-        if (translateNameplatesAndUnitFrames) then
-            nameplateAndUnitFrameTranslator = ns.Translators.NameplateAndUnitFrameTranslator:new()
-            nameplateAndUnitFrameTranslator:SetEnabled(true)
-        end
-        if (translateMovieSubtitles) then
-            movieTranslator = ns.Translators.MovieTranslator:new()
-            movieTranslator:SetEnabled(true)
-        end
+        translator:SetEnabled(true)
+        return translator
     end
 
+    for _, translatorData in ipairs(translators) do
+        if translatorData.isEnabled() then
+            translatorData.translator = createTranslator(translatorData.name, translatorData.args)
+        end
+    end
+end
+
+local function OnAddOnLoaded(_, name)
     if (name == addonName) then
         initializeAddon()
         if not IsLoggedIn() then
