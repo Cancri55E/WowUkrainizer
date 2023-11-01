36456ab1bfe2827d4879273c4ffdc0b507db2c65
Volodymyr 55CancriE Nykonov <cancrigames@gmail.com>
2023-11-01 15:54:36 +0200

    add settings

--- WowUkrainizer.lua
+++ WowUkrainizer.lua
@@ -73,7 +73,7 @@ local translators = {
         name = "QuestTranslator",
         args = nil,
         isEnabled = function()
-            return true
+            return WowUkrainizer_Options.TranslateQuestAndObjectivesFrame
         end
     },
 }
