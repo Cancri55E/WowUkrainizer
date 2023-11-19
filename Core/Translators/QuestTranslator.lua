a72074b62aba3304343d5b0fc4e9de049686206f
Volodymyr 55CancriE Nykonov <cancrigames@gmail.com>
2023-11-19 17:51:31 +0200

    fix: minimap not translated more that one quest, npc or object same time

--- Core/Translators/QuestTranslator.lua
+++ Core/Translators/QuestTranslator.lua
@@ -44,11 +44,24 @@ local UIInfoMessageChange = {
     [ERR_QUEST_ADD_PLAYER_KILL_SII] = {},
 }
 
+local MinimapTooltipCache = {}
 local WorldMapStorylineQuestPinsCache = {}
 local WorldMapChildFramesCache = {}
 
 local translator = class("QuestTranslator", ns.Translators.BaseTranslator)
 ns.Translators.QuestTranslator = translator
+
+local function copyTable(originalTable)
+    local newTable = {}
+    for key, value in pairs(originalTable) do
+        if type(value) == "table" then
+            newTable[key] = copyTable(value)
+        else
+            newTable[key] = value
+        end
+    end
+    return newTable
+end
 
 local function getQuestFrameTranslationOrDefault(default)
     return ns.DbContext.Frames.GetTranslationOrDefault("quest", default)
@@ -921,30 +934,132 @@ local function OnWorldMapPinButtonTooltipUpdated(button) -- original function is
 end
 
 local function OnMinimapMouseoverTooltipPostCall(tooltip, tooltipData)
+    local function processMinimapTooltip(tooltipLine)
+        local result = {}
+        local objectives = {}
+        local substrings = {}
+
+        for substring in tooltipLine:gmatch("[^\n]+") do
+            table.insert(substrings, substring)
+        end
+
+        for i = #substrings, 1, -1 do
+            local substring = substrings[i]
+            if (string.sub(substring, 1, 1) == '-') then
+                substring = string.sub(substring, 3)
+                if string.sub(substring, -2) == '|r' then
+                    substring = string.sub(substring, 1, -3)
+                end
+                table.insert(objectives, substring)
+            elseif (#objectives > 0) then
+                local color = nil
+                local title = string.gsub(substring,
+                    "(.-)%|[cC]([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])",
+                    function(t, c)
+                        color = c
+                        return t
+                    end)
+                table.insert(result,
+                    { type = 'QUEST', title = title, objectives = copyTable(objectives), obejctiveColor = color })
+                objectives = {}
+            else
+                local title = substring
+                local icon = nil
+                local color = nil
+                title = string.gsub(title, "(%|T.-%|t)", function(i)
+                    icon = i
+                    return ''
+                end)
+                title = string.gsub(title,
+                    "%|[cC]([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])(.-)%|[rR]",
+                    function(c, t)
+                        color = c
+                        return t
+                    end)
+                table.insert(result, { type = 'NPC_OR_OBJECT', title = title, icon = icon, color = color })
+            end
+        end
+        return result
+    end
+
     if (tooltipData) then
-        local text = tooltip.TextLeft1:GetText()
-        if (not text) then return end
+        if (#tooltipData.lines > 0) then
+            local text = tooltip.TextLeft1:GetText()
+            if (not text) then return end
 
-        local title = text:gsub("(.*)|cffffffff(.*)", function(t, _) return t end)
+            local hash = ns.StringExtensions.GetHash(text)
+            if (not MinimapTooltipCache[hash]) then
+                local tooltipRows = processMinimapTooltip(text)
 
-        for i = 1, C_QuestLog.GetNumQuestLogEntries() do
-            local questID = C_QuestLog.GetQuestIDForLogIndex(i);
-            if questID and questID > 0 then
-                if (C_QuestLog.GetTitleForQuestID(questID) == title) then
-                    local questLogIndex = C_QuestLog.GetLogIndexForQuestID(questID);
+                local questTitleToQuestIdCache = nil
+                if (tooltipRows) then
+                    for i = 1, #tooltipRows, 1 do
+                        local data = tooltipRows[i]
+                        if (data.type == 'QUEST') then
+                            if (not questTitleToQuestIdCache) then
+                                questTitleToQuestIdCache = {}
+                                for j = 1, C_QuestLog.GetNumQuestLogEntries() do
+                                    local questID = C_QuestLog.GetQuestIDForLogIndex(j);
+                                    if (questID and questID > 0) then
+                                        local questTitle = C_QuestLog.GetTitleForQuestID(questID)
+                                        questTitleToQuestIdCache[questTitle] = questID
+                                    end
+                                end
+                            end
+                            local questID = questTitleToQuestIdCache[data.title]
+                            if (questID) then
+                                local translatedTitle = GetQuestTitle(questID)
+                                if (translatedTitle) then
+                                    data.title = translatedTitle
+                                end
 
-                    local translatedTitle, objectives = GetTranslatedTooltip(nil, questID, questLogIndex)
-                    local textLeft1 = translatedTitle .. HIGHLIGHT_FONT_COLOR_CODE
-                    for i = 1, #objectives, 1 do
-                        textLeft1 = textLeft1 .. "\n" .. objectives[i]
+                                for j = #data.objectives, 1, -1 do
+                                    local objective = data.objectives[j]
+                                    local translatedObjective = GetQuestObjective(questID, objective)
+                                    if (translatedObjective) then
+                                        data.objectives[j] = translatedObjective
+                                    end
+                                end
+                            end
+                        elseif (data.type == 'NPC_OR_OBJECT') then
+                            local translatedTitle = GetUnitNameOrDefault(data.title)
+                            if (translatedTitle == data.title) then
+                                -- TODO: Object Name
+                            end
+                            data.title = translatedTitle
+                        end
                     end
-                    textLeft1 = textLeft1 .. "|r"
 
-                    _G["GameTooltipTextLeft1"]:SetText(textLeft1)
-                    GameTooltip:Show()
-                    break
+                    local translatedTooltip = ''
+                    for i = #tooltipRows, 1, -1 do
+                        local data = tooltipRows[i]
+                        if (data.icon) then
+                            translatedTooltip = translatedTooltip .. data.icon
+                        end
+                        if (data.color) then
+                            translatedTooltip = translatedTooltip .. "|c" .. data.color .. data.title .. "|r"
+                        else
+                            translatedTooltip = translatedTooltip .. data.title
+                        end
+                        if (data.objectives) then
+                            if (data.obejctiveColor) then
+                                translatedTooltip = translatedTooltip .. "|c" .. data.obejctiveColor
+                            end
+                            for j = #data.objectives, 1, -1 do
+                                local objective = data.objectives[j]
+                                translatedTooltip = translatedTooltip .. "\n- " .. objective
+                            end
+                            translatedTooltip = translatedTooltip .. "|r"
+                        end
+                        translatedTooltip = translatedTooltip .. "\n"
+                    end
+
+                    MinimapTooltipCache[hash] = translatedTooltip
                 end
             end
+
+            _G["GameTooltipTextLeft1"]:SetText(MinimapTooltipCache[hash])
+            GameTooltip:Show()
         end
     end
 end
