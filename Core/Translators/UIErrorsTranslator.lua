c990b1ef8592e43a7b24b2bfa8e8fd5f018f994f
Volodymyr 55CancriE Nykonov <cancrigames@gmail.com>
2024-08-31 08:00:49 +0300

    Add missing UI Error Codes for Quests

--- Core/Translators/UIErrorsTranslator.lua
+++ Core/Translators/UIErrorsTranslator.lua
@@ -20,12 +20,119 @@ local ZoneMessages = {
 }
 
 local QuestMessages = {
-    [LE_GAME_ERR_QUEST_OBJECTIVE_COMPLETE_S] = {},
+    [LE_GAME_ERR_QUEST_ALREADY_DONE] = {},
+    [LE_GAME_ERR_QUEST_ALREADY_DONE_DAILY] = {},
+    [LE_GAME_ERR_QUEST_ALREADY_ON] = {},
+    [LE_GAME_ERR_QUEST_FAILED_CAIS] = {},
+    [LE_GAME_ERR_QUEST_FAILED_EXPANSION] = {},
+    [LE_GAME_ERR_QUEST_FAILED_LOW_LEVEL] = {},
+    [LE_GAME_ERR_QUEST_FAILED_MISSING_ITEMS] = {},
+    [LE_GAME_ERR_QUEST_FAILED_NOT_ENOUGH_MONEY] = {},
+    [LE_GAME_ERR_QUEST_FAILED_SPELL] = {},
+    [LE_GAME_ERR_QUEST_FAILED_WRONG_RACE] = {},
+    [LE_GAME_ERR_QUEST_HAS_IN_PROGRESS] = {},
+    [LE_GAME_ERR_QUEST_IGNORED] = {},
+    [LE_GAME_ERR_QUEST_LOG_FULL] = {},
+    [LE_GAME_ERR_QUEST_MUST_CHOOSE] = {},
+    [LE_GAME_ERR_QUEST_NEED_PREREQS] = {},
+    [LE_GAME_ERR_QUEST_ONLY_ONE_TIMED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_ALREADY_ACTIVE] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_ALREADY_JOINED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_ALREADY_MEMBER] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_ALREADY_OWNER] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_BUSY] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_DISABLED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_INVALID_AREA] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_IN_COMBAT] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_IN_PET_BATTLE] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_IN_RAID] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_JOIN_REJECTED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_LEFT] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_LEGACY_LOOT_MODE] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_MEMBER_IN_COMBAT] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_NOT_ACTIVE] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_NOT_IN_PARTY] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NOT_IN_PARTY_S] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_NOT_MEMBER] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_NOT_OWNER] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_QUEST_NOT_COMPLETED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_READY_CHECK_FAILED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_RESTRICTED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_RESTRICTED_CROSS_FACTION] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_RESYNC] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_STARTED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_STOPPED] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_TIMEOUT] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_UNKNOWN] = {},
+    [LE_GAME_ERR_QUEST_UNIGNORED] = {},
     [LE_GAME_ERR_QUEST_UNKNOWN_COMPLETE] = {},
+}
+
+local QuestMessagesWithVariables = {
+    [LE_GAME_ERR_QUEST_NEED_PREREQS_CUSTOM] = {},
+    [LE_GAME_ERR_QUEST_TURN_IN_FAIL_REASON] = {},
+    [LE_GAME_ERR_QUEST_OBJECTIVE_COMPLETE_S] = {},
     [LE_GAME_ERR_QUEST_ADD_KILL_SII] = {},
     [LE_GAME_ERR_QUEST_ADD_FOUND_SII] = {},
     [LE_GAME_ERR_QUEST_ADD_ITEM_SII] = {},
     [LE_GAME_ERR_QUEST_ADD_PLAYER_KILL_SII] = {},
+    [LE_GAME_ERR_QUEST_ACCEPTED_S] = {},
+    [LE_GAME_ERR_QUEST_COMPLETE_S] = {},
+    [LE_GAME_ERR_QUEST_FAILED_BAG_FULL_S] = {},
+    [LE_GAME_ERR_QUEST_FAILED_MAX_COUNT_S] = {},
+    [LE_GAME_ERR_QUEST_FAILED_S] = {},
+    [LE_GAME_ERR_QUEST_FAILED_TOO_MANY_DAILY_QUESTS_I] = {},
+    [LE_GAME_ERR_QUEST_FORCE_REMOVED_S] = {},
+    [LE_GAME_ERR_QUEST_PET_BATTLE_VICTORIES_PVP_II] = {},
+    [LE_GAME_ERR_QUEST_PUSH_ACCEPTED_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_ALREADY_DONE_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_ALREADY_DONE_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_BUSY_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_CLASS_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_CLASS_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_CROSS_FACTION_RESTRICTED_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_DEAD_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_DEAD_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_DECLINED_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_DIFFERENT_SERVER_DAILY_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_DIFFERENT_SERVER_DAILY_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_EXPANSION_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_EXPANSION_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_HIGH_FACTION_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_HIGH_FACTION_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_HIGH_LEVEL_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_HIGH_LEVEL_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_INVALID_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_INVALID_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_LOG_FULL_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_LOG_FULL_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_LOW_FACTION_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_LOW_FACTION_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_LOW_LEVEL_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_LOW_LEVEL_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NEW_PLAYER_EXPERIENCE_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NEW_PLAYER_EXPERIENCE_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NOT_ALLOWED_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NOT_DAILY_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NOT_GARRISON_OWNER_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_ONQUEST_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_ONQUEST_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_PREREQUISITE_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_PREREQUISITE_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_RACE_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_SUCCESS_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_TIMER_EXPIRED_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_TOO_FAR_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_WRONG_COVENANT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_WRONG_COVENANT_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_WRONG_FACTION_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_WRONG_FACTION_TO_RECIPIENT_S] = {},
+    [LE_GAME_ERR_QUEST_REWARD_EXP_I] = {},
+    [LE_GAME_ERR_QUEST_REWARD_MONEY_S] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_OWNER_REFUSED_S] = {},
+    [LE_GAME_ERR_QUEST_SESSION_RESULT_INVALID_OWNER_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_NOT_GARRISON_OWNER_S] = {},
+    [LE_GAME_ERR_QUEST_PUSH_RACE_S] = {},
 }
 
 local function ProcessZoneMessage(messageType, message)
@@ -46,6 +153,13 @@ local function ProcessZoneMessage(messageType, message)
 end
 
 local function ProcessQuestMessage(messageType, message)
+    if (not QuestMessages[messageType]) then return end
+    table.insert(QuestMessages[messageType], { text = message, translatedText = GetTranslatedGlobalString(message) })
+end
+
+local function ProcessQuestMessageWithVariables(messageType, message)
+    if (not QuestMessagesWithVariables[messageType]) then return end
+
     if (messageType == LE_GAME_ERR_QUEST_OBJECTIVE_COMPLETE_S) then
         local text = message
         message:gsub("(.*) %(Complete%)", function(t)
@@ -66,10 +180,8 @@ local function ProcessQuestMessage(messageType, message)
 
         if (not translatedText) then return end
 
-        table.insert(QuestMessages[messageType], { text = message, translatedText = translatedText })
-    end
-
-    if (messageType == LE_GAME_ERR_QUEST_ADD_ITEM_SII or messageType == LE_GAME_ERR_QUEST_ADD_FOUND_SII) then
+        table.insert(QuestMessagesWithVariables[messageType], { text = message, translatedText = translatedText })
+    elseif (messageType == LE_GAME_ERR_QUEST_ADD_ITEM_SII or messageType == LE_GAME_ERR_QUEST_ADD_FOUND_SII) then
         local text = message
         local progress = nil
         message:gsub("(.*): (%d+/%d+)", function(o, p)
@@ -92,10 +204,8 @@ local function ProcessQuestMessage(messageType, message)
 
         if (not translatedText) then return end
 
-        table.insert(QuestMessages[messageType], { text = message, translatedText = translatedText })
-    end
-
-    if (messageType == LE_GAME_ERR_QUEST_ADD_KILL_SII) then
+        table.insert(QuestMessagesWithVariables[messageType], { text = message, translatedText = translatedText })
+    elseif (messageType == LE_GAME_ERR_QUEST_ADD_KILL_SII) then
         local text = message
         local progress = nil
         message:gsub("(.*) slain: (%d+/%d+)", function(o, p)
@@ -118,19 +228,12 @@ local function ProcessQuestMessage(messageType, message)
 
         if (not translatedText) then return end
 
-        table.insert(QuestMessages[messageType], { text = message, translatedText = translatedText })
-    end
-
-
-    if (messageType == LE_GAME_ERR_QUEST_UNKNOWN_COMPLETE) then
-        table.insert(QuestMessages[messageType], { text = message, translatedText = "Завдання виконано." })
-    end
-
-    if (messageType == LE_GAME_ERR_QUEST_ADD_PLAYER_KILL_SII) then
+        table.insert(QuestMessagesWithVariables[messageType], { text = message, translatedText = translatedText })
+    elseif (messageType == LE_GAME_ERR_QUEST_ADD_PLAYER_KILL_SII) then
         local translatedText = message:gsub("Players slain: (%d+/%d+)", function(progress)
             return "Вбито гравців: " .. progress
         end)
-        table.insert(QuestMessages[messageType], { text = message, translatedText = translatedText })
+        table.insert(QuestMessagesWithVariables[messageType], { text = message, translatedText = translatedText })
     end
 end
 
@@ -157,6 +260,7 @@ local function UIErrorsFrame_AddMessage(_, message, _, _, _, _, messageType)
 
     if (ns.SettingsProvider.GetOption(WOW_UKRAINIZER_TRANSLATE_QUEST_AND_OBJECTIVES_FRAME_OPTION)) then
         ProcessQuestMessage(messageType, message)
+        ProcessQuestMessageWithVariables(messageType, message)
     end
 end
 
@@ -166,6 +270,7 @@ local function UIErrorsFrame_OnUpdated()
     end
     if (ns.SettingsProvider.GetOption(WOW_UKRAINIZER_TRANSLATE_QUEST_AND_OBJECTIVES_FRAME_OPTION)) then
         ApplyTranslation(QuestMessages)
+        ApplyTranslation(QuestMessagesWithVariables)
     end
 end
 
